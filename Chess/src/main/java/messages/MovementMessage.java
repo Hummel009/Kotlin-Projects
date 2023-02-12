69b956cc25fe1447f21513cc41754f38a3b7aee8
Hummel009 <emperorbur@gmail.com>
2023-02-12 16:15:59 +0300

    works

--- Chess/src/main/java/messages/MovementMessage.java
+++ Chess/src/main/java/messages/MovementMessage.java
@@ -1,9 +1,21 @@
-package messages;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Messages;
 
-import game.pieces.Coordinate;
+import chess_game.Pieces.Coordinate;
 
+/**
+ *
+ * @author Enes Kızılcın <nazifenes.kizilcin@stu.fsm.edu.tr>
+ */
 
-public class MovementMessage implements java.io.Serializable {
+//This class created for optimizated transformation speed. Because the move class
+//has many reference on it and very heavy. Just keeping coordinates and isKilled value
+// is enough to understood what rival player moved last round.
+public class MovementMessage implements java.io.Serializable{
     public boolean isPieceKilled;
     public Coordinate destinationCoordinate;
     public Coordinate currentCoordinate;
