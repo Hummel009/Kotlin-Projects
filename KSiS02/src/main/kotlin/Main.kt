2aa1f26963adab5402c96965349d07ab06124054
Hummel009 <emperorbur@gmail.com>
2023-02-22 09:41:44 +0300

    починено

--- KSiS02/src/main/kotlin/Main.kt
+++ KSiS02/src/main/kotlin/Main.kt
@@ -1,83 +1,36 @@
-import java.net.DatagramPacket
-import java.net.DatagramSocket
 import java.net.InetAddress
 import java.net.Socket
-import kotlin.random.Random
-
-const val SERVER_PORT = 5000
-const val PACKET_SIZE = 1024
-const val SEND_INTERVAL_MS = 1000
-const val REPORT_INTERVAL_MS = 5000
+import java.util.*
+import kotlin.system.measureTimeMillis
 
 fun main() {
-    val serverAddress = InetAddress.getByName("192.168.100.11")
-    val tcpSocket = Socket(serverAddress, SERVER_PORT)
-    val udpSocket = DatagramSocket()
+    // Установка соединения с сервером
+    val serverAddress = InetAddress.getByName("192.168.43.177")
+    val serverPort = 8080
+    val tcpSocket = Socket(serverAddress, serverPort)
 
-    val sendData = ByteArray(PACKET_SIZE)
-    val receiveData = ByteArray(PACKET_SIZE)
-    val expectedData = ByteArray(PACKET_SIZE)
+    // Генерация случайных данных
+    val random = Random()
+    val dataSize = 1024
+    val data = ByteArray(dataSize)
+    random.nextBytes(data)
 
-    var tcpPacketsSent = 0
-    var tcpPacketsReceived = 0
-    var udpPacketsSent = 0
-    var udpPacketsReceived = 0
-    var udpPacketsLost = 0
-    var startTime = System.currentTimeMillis()
+    // Отправка данных и замер времени передачи
+    val time = measureTimeMillis {
+        val outputStream = tcpSocket.getOutputStream()
+        val inputStream = tcpSocket.getInputStream()
 
-    while (true) {
-        // Generate random data to send
-        Random.nextBytes(sendData)
+        // Отправка данных
+        outputStream.write(data)
+        outputStream.flush()
 
-        // Send data over TCP
-        val tcpOut = tcpSocket.getOutputStream()
-        tcpOut.write(sendData)
-        tcpPacketsSent++
-
-        // Send data over UDP
-        val udpPacket = DatagramPacket(sendData, PACKET_SIZE, serverAddress, SERVER_PORT)
-        udpSocket.send(udpPacket)
-        udpPacketsSent++
-
-        // Receive data over TCP
-        val tcpIn = tcpSocket.getInputStream()
-        tcpIn.read(receiveData)
-        tcpPacketsReceived++
-
-        // Receive data over UDP
-        val udpReceivePacket = DatagramPacket(receiveData, PACKET_SIZE)
-        udpSocket.receive(udpReceivePacket)
-        udpPacketsReceived++
-
-        // Compare received data with expected data
-        if (!receiveData.contentEquals(expectedData)) {
-            udpPacketsLost++
-        }
-
-        // Update the expected data
-        System.arraycopy(sendData, 0, expectedData, 0, PACKET_SIZE)
-
-        // Calculate the transmission rates and lost packets
-        val elapsedTime = System.currentTimeMillis() - startTime
-
-        if (elapsedTime >= REPORT_INTERVAL_MS) {
-            val tcpTransmissionRate = (tcpPacketsReceived.toFloat() / elapsedTime) * 1000
-            val udpTransmissionRate = (udpPacketsReceived.toFloat() / elapsedTime) * 1000
-            val udpPacketLossRate = (udpPacketsLost.toFloat() / udpPacketsSent) * 100
-
-            println("TCP transmission rate: $tcpTransmissionRate packets per second")
-            println("UDP transmission rate: $udpTransmissionRate packets per second")
-            println("UDP packet loss rate: $udpPacketLossRate%")
-
-            tcpPacketsSent = 0
-            tcpPacketsReceived = 0
-            udpPacketsSent = 0
-            udpPacketsReceived = 0
-            udpPacketsLost = 0
-            startTime = System.currentTimeMillis()
-        }
-
-        // Sleep for the send interval
-        Thread.sleep(SEND_INTERVAL_MS.toLong())
+        // Получение результата
+        val result = inputStream.read()
+        println("Результат: $result")
     }
+
+    // Вывод результатов
+    val speed = dataSize / (time / 1000.0)  // скорость в байтах/сек
+    println("Скорость передачи: $speed байт/сек")
+    tcpSocket.close()
 }
