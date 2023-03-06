0e373108acf3add4b7dfa97024d3f36b5ff03358
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-03-06 13:56:00 +0300

    03

---
+++ KSiS03/src/main/kotlin/hummel/DayTimeClient.kt
@@ -0,0 +1,17 @@
+package main.kotlin.hummel
+import java.net.*
+
+fun main() {
+    val serverAddress = "localhost"
+    val serverPort = 13
+
+    val socket = Socket(serverAddress, serverPort)
+    val inputStream = socket.getInputStream()
+    val buffer = ByteArray(4096)
+
+    val length = inputStream.read(buffer)
+    val result = String(buffer, 0, length)
+
+    println("Current time: $result")
+    socket.close()
+}
